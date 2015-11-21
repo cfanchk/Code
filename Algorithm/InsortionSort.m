a1ffbeaf89ae01ef3ebb4e59a7b88a9d5244f44f
cfanchk <chengkaichn@163.com>
2015-11-21 17:27:39 +0800

    First Push

--- Algorithm/InsortionSort.m
+++ Algorithm/InsortionSort.m
@@ -1,3 +1,19 @@
-function InsortionSort()
+function SortArray=InsortionSort(Array)
+    if nargin~=1
+        disp('Invalid Parameter Format!');
+        return;
+    end
 
+    len=length(Array);
+    SortArray=Array;
+    
+    for j=2:len
+        key=SortArray(j);
+        i=j-1;
+        while i>0 && SortArray(i)>key
+            SortArray(i+1)=SortArray(i);
+            i=i-1;
+        end
+        SortArray(i+1)=key;
+    end
 end
