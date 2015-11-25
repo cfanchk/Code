4943a04ca841d856dcc7001babec882aa5f651f2
cfanchk <chengkaichn@163.com>
2015-11-25 22:37:25 +0800

    SIFT corner
    SIFT角点检测部分修改，根据论文确定尺度空间参数，添加金字塔降采样

--- SIFT/Main.m
+++ SIFT/Main.m
@@ -7,13 +7,16 @@ if (numel(size(I))>2)
     I=rgb2gray(I);
 end
 
-DoGImg=sub_DoG(I,8,2);
-num=size(DoGImg,3)/2;
-for i=1:num
-    figure;imshow(I);
-    hold on;
-    [rtemp,ctemp]=find(DoGImg(:,:,2*(i-1)+1));  %极大值点
-    plot(ctemp,rtemp,'r.','MarkerSize',4);
-    [rtemp,ctemp]=find(DoGImg(:,:,2*i));   %极小值点
-    plot(ctemp,rtemp,'g.','MarkerSize',4);
+DoGCorner=sub_DoG(I,3,1.6);
+OctaveNum=size(DoGCorner,2);
+for i=1:OctaveNum
+    [r,c]=size(I);
+    num=size(DoGCorner{1,i},3);
+    for j=1:num
+        figure;imshow(I);
+        hold on;
+        [rtemp,ctemp]=find(DoGCorner{1,i}(:,:,j));
+        plot(ctemp,rtemp,'r.');
+    end
+    I=imresize(I,[floor(r/2) floor(c/2)]);
 end
--- SIFT/sub_DoG.m
+++ SIFT/sub_DoG.m
@@ -1,35 +1,36 @@
-function DoGImg=sub_DoG(I,S,sigma)
-if(S<4)
-    disp('Invalid Input!');
-    return;
-end
-
+function DoGCorner=sub_DoG(I,S,sigma)
 I=double(I);
 [r,c]=size(I);
 k=2^(1/S);
-ScaleImg=zeros(r,c,S);
-MaxDScale=zeros(r,c,S-1);
-MinDScale=zeros(r,c,S-1);
-Maxima=zeros(r,c,S-1);
-Minima=zeros(r,c,S-1);
-DoGImg=zeros(r,c,2*(S-3));
 
-for i=1:S
-    h=fspecial('gaussian',[3 3],sigma*k^(i-1));
-    ScaleImg(:,:,i)=imfilter(I,h,'conv');
-    figure;imshow(uint8(ScaleImg(:,:,i)));
-end
-for i=1:S-1
-    DScaleImg=ScaleImg(:,:,i)-ScaleImg(:,:,i+1);
-    MaxDScale(:,:,i)=ordfilt2(DScaleImg,9,ones(3,3));
-    Maxima(:,:,i)=DScaleImg==MaxDScale(:,:,i);
-    MinDScale(:,:,i)=ordfilt2(DScaleImg,1,ones(3,3));
-    Minima(:,:,i)=DScaleImg==Minima(:,:,i);
-end
+OctaveNum=floor(log2(min(r,c)))-3;
+DoGCorner=cell(1,OctaveNum);
 
-for i=1:S-3
-    DoGImg(:,:,2*(i-1)+1)=Maxima(:,:,i+1) & MaxDScale(:,:,i+1)>MaxDScale(:,:,i) & MaxDScale(:,:,i+1)>MaxDScale(:,:,i+2);
-    DoGImg(:,:,2*i)=Minima(:,:,i+1) & MinDScale(:,:,i+1)<MinDScale(:,:,i) & MinDScale(:,:,i+1)<MinDScale(:,:,i+2);
-end
-
+for j=1:OctaveNum
+    [r,c]=size(I);
+    ScaleImg=zeros(r,c,S+3);
+    MaxDScale=zeros(r,c,S+2);
+    MinDScale=zeros(r,c,S+2);
+    Maxima=zeros(r,c,S+2);
+    Minima=zeros(r,c,S+2);
+    DoGCorner{1,j}=zeros(r,c,S);
+    for i=1:S+3
+        sigmanow=sigma*k^(i-1);
+        masksize=floor(6*sigmanow);
+        h=fspecial('gaussian',[masksize masksize],sigmanow);
+        ScaleImg(:,:,i)=imfilter(I,h,'conv');
+    end
+    for i=1:S+2
+        DScaleImg=ScaleImg(:,:,i+1)-ScaleImg(:,:,i);
+        MaxDScale(:,:,i)=ordfilt2(DScaleImg,9,ones(3,3));
+        Maxima(:,:,i)=DScaleImg==MaxDScale(:,:,i);
+        MinDScale(:,:,i)=ordfilt2(DScaleImg,1,ones(3,3));
+        Minima(:,:,i)=DScaleImg==Minima(:,:,i);
+    end
+    
+    for i=1:S
+        DoGCorner{1,j}(:,:,i)=Maxima(:,:,i+1) & MaxDScale(:,:,i+1)>MaxDScale(:,:,i) & MaxDScale(:,:,i+1)>MaxDScale(:,:,i+2) |...
+            Minima(:,:,i+1) & MinDScale(:,:,i+1)<MinDScale(:,:,i) & MinDScale(:,:,i+1)<MinDScale(:,:,i+2);
+    end
+    I=imresize(ScaleImg(:,:,S+1),[floor(r/2) floor(c/2)]);
 end
